f5c7b159b89d6ad80fe5f968443773b5b1b514bd
Chandu Hima Vamsi <glitch@Chandus-MacBook-Air.local>
2022-10-05 22:05:00 +0530

    Band Name Generator

---
+++ Day1/bandNameGenerator.py
@@ -0,0 +1,8 @@
+#Produce a band name with the inputs from the user
+
+print("\nWelcome to the Band Name Generator!\n")
+
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+print("\nYour band name could be " + city_name + " " + pet_name)
--- Day1/notes.py
+++ Day1/notes.py
@@ -1,1 +1,8 @@
-print("Hi there!")
+print("Hi " + "there!")
+
+# Escape characters - https://www.w3schools.com/python/gloss_python_escape_characters.asp
+print("What\tup!!!\nHomie\b143")
+
+#Input, len function, string formatter
+print(f"Length of your name is" , len(input("What is your name?\n")))
+
