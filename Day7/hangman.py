f31f5aeeeb871a60160b90edc86c6e947b804923
Chandu Hima Vamsi <glitch@Chandus-MacBook-Air.local>
2023-01-13 00:42:37 +0530

    Hangman: Remastered

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -1,65 +1,8 @@
 #Save the fellow creed member - Assassin's Creed Unity
 import random, os
+from hangman_art import HANGMANPICS, logo
 from termcolor import colored
 os.system("clear")
-
-HANGMANPICS = ['''
-  +---+
-      |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
 
 #Word bank
 words = ('mrgod asura theblackspade glitch karma aura zero crazymonk switcher cobra scorpion '
@@ -70,20 +13,22 @@ words = ('mrgod asura theblackspade glitch karma aura zero crazymonk switcher co
 pswd = random.choice(words)
 key = []
 for i in range(len(pswd)): key.append("_")
+print(logo)
 
-for life in range(8):
-    for i in range(len(pswd)): print(key[i], end="")
+for life in range(9):
+    # for i in range(len(pswd)): print(key[i], end="")
     if "_" not in key:
         os.system("clear")
-        print(colored(f'\n\nGood work. Creed owes you one!\n', 'green', attrs=['blink']))
-        print(colored(f'{HANGMANPICS[life]}\n', 'green', attrs=['blink']))
+        print(colored(f'\nGood work. The key is {pswd}. Creed owes you one!', 'light_green', attrs=['blink']))
+        print(colored(f'{HANGMANPICS[life-1]}\n', 'light_green', attrs=['blink']))
         break
     else:
-        if life == 7:
+        if life == 8:
             os.system("clear")
-            print(colored(f'\n\nThe password was {pswd}. Unable to save. Time to avenge!', 'red', attrs=['blink']))
-            print(colored(f'{HANGMANPICS[life]}\n', 'red', attrs=['blink']))
+            print(colored(f"\nUnable to save. The key is {pswd}. It's time to avenge!", 'red', attrs=['blink']))
+            print(colored(f'{HANGMANPICS[life-1]}\n', 'red', attrs=['blink']))
             break
+        for i in range(len(pswd)): print(key[i], end="")
         guess = input("\n\nEnter a letter: ").lower()
         for num in range(len(pswd)):
             if pswd[num] == guess:
