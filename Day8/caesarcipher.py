60198b61927cbf7b5e48764e7ccd43c0cfda3d6d
Chandu Hima Vamsi <glitch@Chandus-MacBook-Air.local>
2023-01-15 19:52:39 +0530

    Caesar Cipher: Remastered

--- Day8/caesarcipher.py
+++ Day8/caesarcipher.py
@@ -6,20 +6,17 @@ from caesarcipher_helper import logo, alphabets
 
 def caesar(message, key, direction):
     result = ""
-    for i in range(0, len(message)):
-        if message[i] in alphabets:
-            if direction == "encode":
-                pivot = alphabets.index(message[i]) + key
-                while pivot > 25: pivot -= 26
-            else:
-                pivot = alphabets.index(message[i]) - key
-                while pivot < 0: pivot += 26
+    if direction == "decode": key *= -1
+    for char in message:
+        if char in alphabets:
+            pivot = alphabets.index(char) + key
+            pivot %= 26
             result += alphabets[pivot]
-        else: result += message[i]
+        else: result += char
     return result
 
 clear()
-type_writer(colored_text(logo, 'light_green'), 0.02)
+type_writer(colored_text(logo), 0.02)
 flag = "yes"
 
 while flag == "yes":
@@ -31,7 +28,7 @@ while flag == "yes":
     else:
         message = input("\nEnter the message: ")
         key = int(input("\nEnter shift amount: "))
-        print(colored_text(f"\n{direction.capitalize()}d message is {caesar(message, key, direction)}"))
+        print(colored_text(f"\n{direction.capitalize()}d message is {caesar(message, key % 26, direction)}"))
 
     flag = input("\nType 'yes' if you want to go again, else type 'no': ").lower()
 
