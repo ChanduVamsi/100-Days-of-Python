a2e47ea96545e7802be45d317207a78ab9c3e0d1
Chandu Hima Vamsi <glitch@Chandus-MacBook-Air.local>
2023-01-29 23:23:03 +0530

    Utility Functions

--- util/helper.py
+++ util/helper.py
@@ -4,13 +4,17 @@ from time import sleep
 from termcolor import colored
 
 def clear():
+	"""Clears the output terminal"""
 	os.system("clear")
 
 def type_writer(text, speed = 0.02):
+	"""Prints the output in a typewriter format"""
 	for char in text:
 		sleep(speed)
 		sys.stdout.write(char)
 		sys.stdout.flush()
 
 def colored_text(text, color = "light_green"):
+	"""Returns the colored output. 
+	Default is neon green"""
 	return colored(text, color, attrs=['blink'])
