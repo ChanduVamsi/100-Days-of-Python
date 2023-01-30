435f3b5a209b59cf2fb3f2f1b3f2ffb2172d117e
Chandu Hima Vamsi <glitch@Chandus-MacBook-Air.local>
2023-01-31 01:40:16 +0530

    Blackjack

--- Day11/blackjack_helper.py
+++ Day11/blackjack_helper.py
@@ -1,3 +1,8 @@
+import sys, random
+sys.path.append("")
+
+from util.helper import clear
+
 logo = '''\n
  _     _            _    _            _    
 | |   | |          | |  (_)          | |   
@@ -25,3 +30,34 @@ cards = {
     "Q" : 10, 
     "K" : 10
 }
+
+def showCards(all_cards):
+    '''Prints all the cards in the player's deck'''
+    for card in all_cards: print(f" {card}", end="")
+
+def getSum(all_cards):
+    '''Returns the sum of the cards for the concerned player'''
+    sum = 0
+    for card in all_cards: sum += cards[card]
+    if sum > 21 and "A" in all_cards: sum -= 10
+    return sum
+
+def hit():
+    '''Adds a new card to the player's deck'''
+    return random.choice(list(cards.keys()))
+
+def cardDeck(flag, user_cards, user_sum, comp_cards, comp_sum):
+    '''Shows the current status of the players decks'''
+    clear()
+    print(logo, end ="")
+    print("\nUSER:", end="")
+    showCards(user_cards)
+    print(f" -> {user_sum}")
+    if flag: 
+        print("\nCOMP:", end="")
+        showCards(comp_cards)
+        print(f" -> {comp_sum}\n")
+    else: 
+        print(f"\nCOMP: {comp_cards[0]}", end="")
+        for i in range(1, len(comp_cards)): print(" _", end = "")
+        print(" -> ?")
