6359725efabd0a43280d578b757b4de8e0226a7a
Chandu Hima Vamsi <glitch@Chandus-MacBook-Air.local>
2023-02-16 02:57:02 +0530

    Fun little games with python and more...

---
+++ Pet_Projects/catgpt_clone.py
@@ -0,0 +1,23 @@
+import sys, random
+sys.path.append("")
+
+from util.helper import clear, colored_text, type_writer
+
+def meow():
+    meom = "Meow"
+    for _ in range(random.randint(1, 10)): meom += " meow"
+    return meom + random.choice(['.', '!', '?'])
+
+logo = '''   ______      __  __________  ______
+  / ____/___ _/ /_/ ____/ __ \/_  __/
+ / /   / __ `/ __/ / __/ /_/ / / /   
+/ /___/ /_/ / /_/ /_/ / ____/ / /    
+\____/\__,_/\__/\____/_/     /_/     
+'''
+
+clear()
+print(colored_text(logo), end="")
+print(colored_text("\n   What if ChatGPT were a cat?\n"))
+while True:
+    type_writer(f"\n🐱: {colored_text(meow())}")
+    input(f"\n\n🤷: ")
